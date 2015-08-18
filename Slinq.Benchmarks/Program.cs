63355444db38e645cdf48c1d76c86fee59a5f867
Adam Sitnik <adam.sitnik@gmail.com>
2015-08-18 18:21:54 +0200

    added FxCop and StyleCop, warnings as errors

--- Slinq.Benchmarks/Program.cs
+++ Slinq.Benchmarks/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace Slinq.Benchmarks
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
